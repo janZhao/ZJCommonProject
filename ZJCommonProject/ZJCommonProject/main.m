8bc6f64d5d75a79ae7497429b5c79bac8d778bd6
janZhao <zhaojian3513@163.com>
2016-01-27 11:31:46 +0800

    init Project

---
+++ ZJCommonProject/ZJCommonProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJCommonProject
+//
+//  Created by jyd on 16/1/27.
+//  Copyright © 2016年 jyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ZJCommonProject/ZJCommonProjectTests/ZJCommonProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  ZJCommonProjectTests.m
+//  ZJCommonProjectTests
+//
+//  Created by jyd on 16/1/27.
+//  Copyright © 2016年 jyd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZJCommonProjectTests : XCTestCase
+
+@end
+
+@implementation ZJCommonProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
